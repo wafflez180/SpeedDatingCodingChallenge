0c62145bb0039f0fced554880aff200530906a4f
Arthur De Araujo <arthur@indierugs.com>
2025-03-24 12:58:24 -0400

    Initial Commit

---
+++ SpeedDatingCodingChallenge/SpeedDatingCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedDatingCodingChallengeApp.swift
+//  SpeedDatingCodingChallenge
+//
+//  Created by Arthur De Araujo on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SpeedDatingCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpeedDatingCodingChallengeTests/SpeedDatingCodingChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedDatingCodingChallengeTests.swift
+//  SpeedDatingCodingChallengeTests
+//
+//  Created by Arthur De Araujo on 3/24/25.
+//
+
+import Testing
+@testable import SpeedDatingCodingChallenge
+
+struct SpeedDatingCodingChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
